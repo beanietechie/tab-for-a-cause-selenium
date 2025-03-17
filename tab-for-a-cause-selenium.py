f58bed5225d48fe667e562c8ebadabf8b6b0804b
BeanieTechie <beanietechie@gmail.com>
2025-03-17 16:56:40 -0500

    rearrange a bit and rewrite some outputs

--- tab-for-a-cause-selenium.py
+++ tab-for-a-cause-selenium.py
@@ -5,19 +5,22 @@ from selenium.common.exceptions import WebDriverException
 from time import sleep
 from sys import stdout
 
+stdout.write('=== tab-for-a-cause-selenium ===\n')
+
 credentialsFile = open('credentials.txt', 'r')
 email = credentialsFile.readline().strip()
 password = credentialsFile.readline().strip()
-
-stdout.write('=== tab-for-a-cause-selenium ===\n')
+stdout.write('credentials read\n')
 
 driver = webdriver.Firefox()
+stdout.write('browser started\n')
 
 driver.get('https://tab.gladly.io/newtab/')
+stdout.write('tab for a Cause page reached\n')
 
 wait = WebDriverWait(driver, 15)
 wait.until(lambda driver: driver.current_url == 'https://tab.gladly.io/newtab/auth/?app=tab') # wait for login page to load
-stdout.write('login page reached, attempting to login\n')
+stdout.write('Login page reached, attempting to login.\n')
 
 loginBtn = wait.until(lambda driver: driver.find_element(By.CLASS_NAME, 'firebaseui-idp-password')) # wait for login button to load
 loginBtn.click()
@@ -32,7 +35,7 @@ submitBtn.click()
 stdout.write('credentials submitted\n')
 
 wait.until(lambda driver: driver.current_url == 'https://tab.gladly.io/newtab/') # wait for new tab page to load
-stdout.write('new tab page reached\n')
+stdout.write('Tab for a Cause new tab page reached\n')
 
 counter = 0
 while True:
@@ -42,4 +45,4 @@ while True:
     except WebDriverException:
         None
     counter += 1
-    stdout.write('\rrefreshed (%i)' % counter)
+    stdout.write('\rrefreshed %i time(s)' % counter)
