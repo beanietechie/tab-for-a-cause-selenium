0077839a9cfa8f602ebc36a4eb5f9749f6022833
BeanieTechie <beanietechie@gmail.com>
2025-03-18 02:35:36 -0500

    replace stdout with print as stdout-specific features are no longer in use

--- tab-for-a-cause-selenium.py
+++ tab-for-a-cause-selenium.py
@@ -3,25 +3,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 from time import sleep
-from sys import stdout
+import os
 
 def main():
-    stdout.write('=== tab-for-a-cause-selenium ===\n')
+    print('=== tab-for-a-cause-selenium ===')
 
     credentialsFile = open('credentials.txt', 'r')
     email = credentialsFile.readline().strip()
     password = credentialsFile.readline().strip()
-    stdout.write('credentials read\n')
+    print('credentials read')
 
     driver = webdriver.Firefox()
-    stdout.write('browser started\n')
+    print('browser started')
 
     driver.get('https://tab.gladly.io/newtab/')
-    stdout.write('tab for a cause page reached\n')
+    print('tab for a cause page reached')
 
     wait = WebDriverWait(driver, 15)
     wait.until(lambda driver: driver.current_url == 'https://tab.gladly.io/newtab/auth/?app=tab') # wait for login page to load
-    stdout.write('Login page reached, attempting to login.\n')
+    print('Login page reached, attempting to login.')
 
     loginBtn = wait.until(lambda driver: driver.find_element(By.CLASS_NAME, 'firebaseui-idp-password')) # wait for login button to load
     loginBtn.click()
@@ -33,17 +33,17 @@ def main():
     passwordTxt.send_keys(password)
     submitBtn = wait.until(lambda driver: driver.find_element(By.CLASS_NAME, 'firebaseui-id-submit')) # wait for submit button to load
     submitBtn.click()
-    stdout.write('credentials submitted\n')
+    print('credentials submitted')
 
     wait.until(lambda driver: driver.current_url == 'https://tab.gladly.io/newtab/') # wait for new tab page to load
-    stdout.write('tab for a cause new tab page reached\n')
+    print('tab for a cause new tab page reached')
 
     while True:
         sleep(4)
         try:
             driver.refresh()
         except WebDriverException:
-            stdout.write('refresh failed, continuing, if you see this repeatedly try restarting the program or check your internet connection\n')         
+            print('refresh failed, continuing, if you see this repeatedly try restarting the program or check your internet connection')         
 
 if __name__ == '__main__':
     main()
